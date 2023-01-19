c6c726aa7ac00cbf6fcc8182664f119f022f0387
durmus ali saymaz <dali@durmuss-MacBook-Pro.local>
2023-01-19 10:45:48 +0100

    4 tane daha yaptim

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 import { fishFarm } from "./fishFarm.js";
-import { fishStock500Over, fishPrice9to12,fishBeWinter,fishEntriExDate} from "./function.js";
+import { fishStock500Over, fishPrice9to12, fishBeWinter, fishEntriExDate, alphabeticalOrderTheFishFromEUandUnder10Fr,totalFishStock,mostExpensiveFish,mostStandFishfromCountry} from "./function.js";
 
 console.log(`5000 kilodan fazla olana blaiklar \n` +fishStock500Over(fishFarm))
 console.log(`9 ila 12 arasinda olana baliklar \n`+fishPrice9to12(fishFarm))
 console.log(`Be ve Kis arasinda olana baliklar \n`+fishBeWinter(fishFarm))
 console.log(`baliklari son kullanma tarihine gore siralanmis hali \n`+fishEntriExDate(fishFarm))
+
+console.log(`(AB) gelen ve fiyati 10Fr dan dusuk olan baliklari alfabetik siraya\n`+alphabeticalOrderTheFishFromEUandUnder10Fr(fishFarm))
+console.log(`Toplam balik stoku \n`+totalFishStock(fishFarm))
+console.log(` En pahali olan balik \n`+mostExpensiveFish(fishFarm))
+console.log(`En uzun sureli durabilen (birden fazla olabilir)baliklar \n`+mostStandFishfromCountry(fishFarm))
--- function.js
+++ function.js
@@ -12,6 +12,7 @@
 // 11) Yazlik sezonda cikan ve AB disindan gelen ve de ZH'de satilan baliklarin ortalama gramajini bulunuz?
 
 
+// 1) Stok miktari 500 kg uzerinde olan baliklarin isimleri nelerdir?
 const fishStock500Over = (pParams) => {
   return pParams
 
@@ -20,6 +21,7 @@ const fishStock500Over = (pParams) => {
     .join(``)
 }
 
+// 2) Fiyat araligi 9Fr. ile 12 Fr. arasindaki baliklar hangileridir?
 const fishPrice9to12 = (pParams) => {
   return pParams
     .filter((fish) => fish.price > 9 && fish.price < 12)
@@ -27,7 +29,7 @@ const fishPrice9to12 = (pParams) => {
     .join(``)
 
 }
-
+// 3) Sadece Bern'de ve kis sezonu satilan baliklar hangileridir?
 const fishBeWinter = (pParams) => {
   return pParams
     .filter((fish) => fish.saleLocations.includes("BE") && fish.season === "Winter")
@@ -35,6 +37,7 @@ const fishBeWinter = (pParams) => {
     .join(`\n`)
 }
 
+// 4) Son kullanma tarihlerine gore baliklari siralayiniz. (Son kullanma tarihi yaklasan baliklar once gosterilmelidir)
 const fishEntriExDate = (pParams) => {
   return pParams
     .map((fish) => {
@@ -48,5 +51,58 @@ const fishEntriExDate = (pParams) => {
     .join(`\n`)
 }
 
-export { fishStock500Over, fishPrice9to12, fishBeWinter, fishEntriExDate }
+// 5) Avrupa Birligi'nden (AB) gelen ve fiyati 10Fr dan dusuk olan baliklari alfabetik siraya gore siralayiniz.
+const alphabeticalOrderTheFishFromEUandUnder10Fr = (pArr) => {
+  return pArr
+    .filter(
+      (fish) =>
+        fish.price < 10 &&
+        (fish.originCountry === "Poland" ||
+          fish.originCountry === "France" ||
+          fish.originCountry === "Italy" ||
+          fish.originCountry === "GREECE" ||
+          fish.originCountry === "Spain")
+    )
+    .map((fish) => `${fish.fishType}\n`)
+    .sort()
+    .join(``);
+};
+
+// 6) Toplam balik stoku ne kadardir?
+const totalFishStock = (pArr) => {
+  return pArr
+    .filter((fish) => fish.stockVolumeInKg)
+    .map((fish) => fish.stockVolumeInKg)
+    .reduce((acc, curr) => acc + curr);
+};
+
+// 7) En pahali olan balik hangisidir?
+const mostExpensiveFish = (pArr) => {
+  // the prices are sorted with sort method and with map method are written price and fish type
+  return pArr
+    .sort((a, b) => b.price - a.price)
+    .map((fish) => `${fish.price}CHF ${fish.fishType}`)[0];
+};
+
+// 8) En uzun sureli durabilen baliklar hangi ulkeden gelmektedir?
+const mostStandFishfromCountry = (pArr) => {
+  let highDurationDay = [];
+  highDurationDay.push(
+    pArr
+      .sort((a, b) => b.durationInDays - a.durationInDays)
+      .map((day) => day.durationInDays)[0]
+  );
+  return pArr
+    .filter((fish) => fish.durationInDays == highDurationDay)
+    .map(
+      (fish) =>
+        `${fish.durationInDays} ${fish.fishType} from ${fish.originCountry}`
+    )
+    .join(`\n`);
+};
+
+// 9) Kis ve sonbahar sezonu icin swiss romande region'da satilan baliklarin ortalama fiyati nedir?
+
+
+export { fishStock500Over, fishPrice9to12, fishBeWinter, fishEntriExDate, alphabeticalOrderTheFishFromEUandUnder10Fr,totalFishStock,mostExpensiveFish,mostStandFishfromCountry}
 
